9750950c64eb17f27679cbea28ad33875581df00
kishan04rajput <kishan9rajput@gmail.com>
2025-09-18 20:31:17 +0530

    Enhance UI components with new styles and functionality; add display mode toggle to ChartTableBlock and improve layout in LocationDateBlock and App.css

--- src/components/ChartTableBlock.js
+++ src/components/ChartTableBlock.js
@@ -1,11 +1,57 @@
+import { useState } from "react";
+import "./ChartTableBlock.css";
 const ChartTableBlock = ({ data }) => {
+    const [displayMode, setDisplayMode] = useState("chart");
+
+    const displayChart = () => {
+        if (displayMode === "chart") {
+            return;
+        }
+        setDisplayMode("chart");
+        document.getElementById("chart-button").classList.add("active-button");
+        document
+            .getElementById("table-button")
+            .classList.remove("active-button");
+    };
+    const displayTable = () => {
+        if (displayMode === "table") {
+            return;
+        }
+        setDisplayMode("table");
+        document.getElementById("table-button").classList.add("active-button");
+        document
+            .getElementById("chart-button")
+            .classList.remove("active-button");
+    };
     return (
         <div>
             {data && (
-                <div>
-                    <h1>Chart</h1>
-                    <h1>Table</h1>
-                    <p>{JSON.stringify(data)}</p>
+                <div id="display-mode-block">
+                    <div id="display-mode-buttons">
+                        <span
+                            onClick={displayChart}
+                            className="button"
+                            id="chart-button"
+                        >
+                            Chart
+                        </span>
+                        <span
+                            onClick={displayTable}
+                            className="button"
+                            id="table-button"
+                        >
+                            Table
+                        </span>
+                    </div>
+                    {displayMode === "chart" ? (
+                        <div>
+                            <h1>Chart</h1>
+                        </div>
+                    ) : (
+                        <div>
+                            <h1>Table</h1>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
