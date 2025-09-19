ade10235d10921d194ef29249845c66ad5ebbca4
kishan04rajput <kishan9rajput@gmail.com>
2025-09-19 22:33:34 +0530

    Refactor FooterComponent to remove unnecessary margin for improved layout consistency

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,6 +1,6 @@
 export const FooterComponent = () => {
     return (
-        <footer className="mt-8 sm:mt-12 bg-white p-4 sm:p-6 rounded-xl border border-gray-200 shadow-md text-center">
+        <footer className="sm:mt-12 bg-white p-4 sm:p-6 rounded-xl border border-gray-200 shadow-md text-center">
             <p className="text-gray-600">
                 &copy; {new Date().getFullYear()} Weather Viewer. All rights
                 reserved.
