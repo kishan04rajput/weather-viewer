eff956b048cf0cd00f71d76b753b220c70acc610
kishan04rajput <kishan9rajput@gmail.com>
2025-09-20 00:01:36 +0530

    Update TableComponent for improved styling and pagination handling
    - Centered the "Date" header for better alignment and visual consistency.
    - Adjusted pagination input handling to accept string values directly, enhancing user experience.

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -218,7 +218,7 @@ export const TableComponent = ({ data }) => {
                         <thead className="bg-gray-50">
                             <tr>
                                 <th
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
                                     rowSpan={2}
                                 >
                                     Date
@@ -363,7 +363,7 @@ export const TableComponent = ({ data }) => {
                                                 value={page}
                                                 onChange={(e) =>
                                                     handleJumpToPage(
-                                                        parseInt(e.target.value)
+                                                        e.target.value
                                                     )
                                                 }
                                             />
